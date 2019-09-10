6720a88dbe64905d585119d2279e64cb46b7e6d8
Vladyslav Samoilenko <kekonmonday@gmail.com>
2019-09-10 22:50:39 +0300

    add test examples

--- src/laba_2/Main.java
+++ src/laba_2/Main.java
@@ -4,9 +4,6 @@ import laba_2.encryption.*;
 import laba_2.helper.ConsoleHelper;
 import laba_2.helper.EncryptionHelper;
 
-import java.util.ArrayList;
-import java.util.TreeMap;
-
 public class Main {
 
     private static final String WORD_TO_ENCRYPT_1 = "САМОЙЛЕНКО";
@@ -17,36 +14,42 @@ public class Main {
 
     private static final String WORD_TO_ENCRYPT_4 = "САМОЙЛЕНКО ВЛАД";
 
+    private static final String TEST_EXAMPLE_1 = "КЛАВІАТУРА";
+
+    private static final String TEST_EXAMPLE_2 = "ПУСТОПОРОЖНІЙ";
+
+
     public static void main(String[] args) {
-        String result = new SimpleSinglePermutationEncryption(WORD_TO_ENCRYPT_1, "2" + EncryptionHelper.SPLIT_BY + "3" + EncryptionHelper.SPLIT_BY + "10"
+        String result = new SimpleSinglePermutationEncryption(TEST_EXAMPLE_2, "2" + EncryptionHelper.SPLIT_BY + "3" + EncryptionHelper.SPLIT_BY + "10"
                 + EncryptionHelper.SPLIT_BY + "8" + EncryptionHelper.SPLIT_BY + "6" + EncryptionHelper.SPLIT_BY + "7" + EncryptionHelper.SPLIT_BY + "1"
-                + EncryptionHelper.SPLIT_BY + "9" + EncryptionHelper.SPLIT_BY + "5" + EncryptionHelper.SPLIT_BY + "4").execute();
+                + EncryptionHelper.SPLIT_BY + "9" + EncryptionHelper.SPLIT_BY + "5" + EncryptionHelper.SPLIT_BY + "13" + EncryptionHelper.SPLIT_BY + "11"
+                + EncryptionHelper.SPLIT_BY + "4" + EncryptionHelper.SPLIT_BY + "12").execute();
         ConsoleHelper.printResultString();
         System.out.println(result);
         System.out.println(System.lineSeparator());
-        result = new BlockSinglePermutationEncryption(WORD_TO_ENCRYPT_1, "2" + EncryptionHelper.SPLIT_BY + "1").execute();
+        result = new BlockSinglePermutationEncryption(TEST_EXAMPLE_2, "2" + EncryptionHelper.SPLIT_BY + "3" + EncryptionHelper.SPLIT_BY + "1").execute();
         ConsoleHelper.printResultString();
         System.out.println(result);
         System.out.println(System.lineSeparator());
-        result = new TableRoutePermutationEncryption(WORD_TO_ENCRYPT_2).execute();
+        result = new TableRoutePermutationEncryption(TEST_EXAMPLE_2).execute();
         ConsoleHelper.printResultString();
         System.out.println(result);
         System.out.println(System.lineSeparator());
-        result = new VerticalPermutationEncryption(WORD_TO_ENCRYPT_3, "1" + EncryptionHelper.SPLIT_BY + "5" + EncryptionHelper.SPLIT_BY + "3"
+        result = new VerticalPermutationEncryption(TEST_EXAMPLE_2, "1" + EncryptionHelper.SPLIT_BY + "5" + EncryptionHelper.SPLIT_BY + "3"
                 + EncryptionHelper.SPLIT_BY + "2" + EncryptionHelper.SPLIT_BY + "4").execute();
         ConsoleHelper.printResultString();
         System.out.println(result);
         System.out.println(System.lineSeparator());
-        result = new RotaryLatticeEncryption(WORD_TO_ENCRYPT_4).execute();
+        result = new RotaryLatticeEncryption(TEST_EXAMPLE_2).execute();
         ConsoleHelper.printResultString();
         System.out.println(result);
         System.out.println(System.lineSeparator());
-        result = new MagicSquareEncryption(WORD_TO_ENCRYPT_4).execute();
+        result = new MagicSquareEncryption(TEST_EXAMPLE_2).execute();
         ConsoleHelper.printResultString();
         System.out.println(result);
         System.out.println(System.lineSeparator());
-        result = new DoublePermutationEncryption(WORD_TO_ENCRYPT_3, "2" + EncryptionHelper.SPLIT_BY + "3" + EncryptionHelper.SPLIT_BY + "1" + EncryptionHelper.SPLIT_BY + "4",
-                "3" + EncryptionHelper.SPLIT_BY + "4" + EncryptionHelper.SPLIT_BY + "1" + EncryptionHelper.SPLIT_BY + "2" + EncryptionHelper.SPLIT_BY + "5").execute();
+        result = new DoublePermutationEncryption(TEST_EXAMPLE_2, "2" + EncryptionHelper.SPLIT_BY + "3" + EncryptionHelper.SPLIT_BY + "1" + EncryptionHelper.SPLIT_BY + "4",
+                "3" + EncryptionHelper.SPLIT_BY + "1" + EncryptionHelper.SPLIT_BY + "2" + EncryptionHelper.SPLIT_BY + "4").execute();
         ConsoleHelper.printResultString();
         System.out.println(result);
         System.out.println(System.lineSeparator());
